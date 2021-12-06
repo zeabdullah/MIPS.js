53e127dbbe4ae1bc1e971c8a4abbc2a8546efdd5
abdullahzeidan <58212246+AbdullahZeidan@users.noreply.github.com>
2021-12-06 11:39:37 +0200

    Add: ➕ Added line-reader dependency
    - Added instructionList.js to contain the map to all instruction opcodes

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-import * as fs from 'fs';
-import { registerFile, specialRegisterFile } from './registerFile.js';
+import * as lineReader from 'line-reader';
+// import { registerFile, specialRegisterFile } from './src/registerFile';
 
 // read .testfile
-const sampleTxtFile = fs.readFileSync('.TESTFILE', 'utf-8');
+lineReader.eachLine('.TESTFILE', (line, last) => {
+  console.log(line);
+});
 
 // e.g.: addi $t0, $s0, $s1
 // steps: split string into the instruction (addi) and 'rest' ($t0, $s0, $s1)
---
+++ src/instructionList.js
@@ -0,0 +1,17 @@
+const instructions = {
+  add: {
+    instructionFunction: add
+  },
+  addi: {
+    instructionFunction: add
+  },
+  addu: {
+    instructionFunction: addUnsigned
+  },
+  and: {},
+  andi: {},
+  or: {},
+  ori: {},
+  sll: {},
+  srl: {}
+};
