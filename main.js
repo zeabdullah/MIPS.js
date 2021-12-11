7af9f814b7771252521354560c2cb2d8bbe184d9
abdullahzeidan <58212246+AbdullahZeidan@users.noreply.github.com>
2021-12-11 23:12:41 +0200

    Refactor: 🎨 `main.js`
    - Added a function to check for comments (not fully functional)
    - Commented in a concept for checking and storing labels
      -- Useful if we're able to to branches and jumps later

--- main.js
+++ main.js
@@ -9,11 +9,40 @@ function main() {
     return;
   }
 
+  // const labels = [];
+  let lineNum = 1;
   lineReader.eachLine(process.argv[2], (line, last) => {
+    // Next line if current line is empty or is a comment
+    if (!line || isAComment(line)) return;
+
+    // if (isALabel(line)) {
+    //   storeLabel(line)
+    //   labels.push({ label: , lineNum });
+    // }
+
     const lineSeparated = line.split(' ');
 
     const opcode = lineSeparated[0];
     const operands = lineSeparated.slice(1).join('');
     dispatchInstruction(opcode, operands);
+
+    lineNum++;
   });
+
+  // function storeLabel(line) {
+  //   const splitLine = line.split('');
+
+  // }
 }
+
+/* Basic (and not complete) checking if a line is a comment.
+  Doesn't handle comments that don't come at the start. */
+function isAComment(line) {
+  return line[0] === '#' || line.slice(0, 2) === '//' ? true : false;
+}
+
+// Checks if a given line is a MIPS assembly label
+// function isALabel(line) {
+//   const splitLine = line.split(' ');
+//   return splitLine[0] && splitLine[1] === ':' && splitLine.length === 2 ? true : false;
+// }
